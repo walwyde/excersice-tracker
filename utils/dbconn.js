be363263c7c7024df2f7ddb79e2642043fe479bb
walwyde <walwyde@gmail.com>
2023-10-06 11:20:19 +0100

    initial commit

--- utils/dbconn.js
+++ utils/dbconn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/exercise_tracker", {
+mongoose.connect("process.env.MONGO_URI", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
